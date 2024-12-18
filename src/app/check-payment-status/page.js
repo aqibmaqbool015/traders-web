5733f54731007fafe75d0351f4082a31a519508f
aqibmaqbool015 <engnraqib1122@gmail.com>
2024-12-18 22:25:37 +0500

    Update again and again

--- src/app/check-payment-status/page.js
+++ src/app/check-payment-status/page.js
@@ -6,7 +6,6 @@ import { checkPaymentStatusApi, updatePremiumUser } from "./api";
 import { getUserProfile } from "../login/api";
 import Image from "next/image";
 import { toast, ToastContainer } from "react-toastify";
-c
 import CustomToast from "../components/toast";
 
 const Success = () => {
